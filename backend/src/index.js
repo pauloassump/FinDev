048ac8089cfc2f45953863513c0df9061f674482
pauloassump <paulo.henriquejcn@gmail.com>
2020-02-11 13:50:04 -0300

    semana_omnistack_10.0

---
+++ backend/src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://admin:admin@cluster0-uyxsr.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+})
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333)
---
+++ web/src/components/DevItem/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function DevItem({ dev }) {
+  return (
+    <li className="dev-item">
+          <header>
+            <img src={dev.avatar_url} alt={dev.name}/>
+            <div className="user-info"></div>
+            <strong>{dev.name}</strong>
+            <span>{dev.techs.join(', ')}</span>
+          </header>
+          <p>{dev.bio}</p>
+          <a href={`https://github.com/${dev.github_username}`}>Acessar Perfil no Github</a>
+        </li>
+  )
+}
+
+export default DevItem;
